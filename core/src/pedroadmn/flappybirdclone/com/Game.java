d0dd4e2edc07240b7b506addf338c6517f82f3c2
Pedro Adelino de Mello Neto <pedro.mello@virtus.ufcg.edu.br>
2021-04-07 09:50:38 -0300

    adding bird fly animation

--- core/src/pedroadmn/flappybirdclone/com/Game.java
+++ core/src/pedroadmn/flappybirdclone/com/Game.java
@@ -10,17 +10,23 @@ public class Game extends ApplicationAdapter {
 	private int xMoviment = 0;
 	private int yMoviment = 0;
 	private SpriteBatch batch;
-	private Texture bird;
+	private Texture[] birds;
 	private Texture background;
 
 	// Config
 	private float deviceWidth;
 	private float deviceHeight;
+	private float variation = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		bird = new Texture("passaro1.png");
+
+		birds = new Texture[3];
+		birds[0] = new Texture("passaro1.png");
+		birds[1] = new Texture("passaro2.png");
+		birds[2] = new Texture("passaro3.png");
+
 		background = new Texture("fundo.png");
 
 		deviceWidth = Gdx.graphics.getWidth();
@@ -30,10 +36,17 @@ public class Game extends ApplicationAdapter {
 	@Override
 	public void render () {
 		batch.begin();
+
+		if (variation > 3) {
+			variation = 0;
+		}
+
 		batch.draw(background, 0, 0, deviceWidth, deviceHeight);
+		batch.draw(birds[(int)variation], 30, deviceHeight / 2);
+
+		variation += Gdx.graphics.getDeltaTime() * 10;
 		xMoviment++;
 		yMoviment++;
-		batch.draw(bird, xMoviment, 500);
 		batch.end();
 	}
 	
